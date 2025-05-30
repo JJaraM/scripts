d3e99b65694b5c3fbfc656d9cfcda3c9b958c3ec
Jonathan Jara Morales <jonathan.jara.morales@gmail.com>
2025-05-30 05:49:37 +0100

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -1,1 +1,20 @@
 
+#!/bin/bash
+# The following script allows you to enable ssh.
+#
+# @author jonathan jara morales
+# @since 2025-05-03
+
+# Install SSH
+sudo apt update
+sudo apt install openssh-server -y
+
+# Verify service is up and running
+sudo systemctl status ssh
+
+# Enable it
+sudo systemctl start ssh
+sudo systemctl enable ssh
+
+# Open firewall
+sudo ufw allow 22/tcp
